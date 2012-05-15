65ed232f46c1402bdf7680ffa01198f8231751bb
Rein Raudjärv <rein@zeroturnaround.com>
2012-05-15 15:17:51 +0300

    Added methods for transforming ZIP entries.

---
+++ src/main/java/org/zeroturnaround/zip/transform/ByteArrayZipEntryTransformer.java
@@ -0,0 +1,25 @@
+package org.zeroturnaround.zip.transform;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.zeroturnaround.zip.ByteSource;
+
+public abstract class ByteArrayZipEntryTransformer implements ZipEntryTransformer {
+
+  /**
+   * Transforms the given byte array into a new one.
+   */
+  protected abstract byte[] transform(ZipEntry zipEntry, byte[] input) throws IOException;
+
+  public void transform(InputStream in, ZipEntry zipEntry, ZipOutputStream out) throws IOException {
+    byte[] bytes = IOUtils.toByteArray(in);
+    bytes = transform(zipEntry, bytes);
+    ByteSource source = new ByteSource(zipEntry.getName(), bytes);
+    ZipEntrySourceZipEntryTransformer.addEntry(source, out);
+  }
+
+}
