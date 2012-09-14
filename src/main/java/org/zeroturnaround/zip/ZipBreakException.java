08b0260ee98511156a4a1467ac8535662fca3914
Toomas Romer <toomasr@gmail.com>
2012-09-14 11:45:04 +0300

    Added some easy constructors

--- src/main/java/org/zeroturnaround/zip/ZipBreakException.java
+++ src/main/java/org/zeroturnaround/zip/ZipBreakException.java
@@ -1,5 +1,15 @@
 package org.zeroturnaround.zip;
 
 public class ZipBreakException extends RuntimeException {
+  public ZipBreakException(String msg) {
+    super(msg);
+  }
 
+  public ZipBreakException(Exception e) {
+    super(e);
+  }
+
+  public ZipBreakException() {
+    super();
+  }
 }
