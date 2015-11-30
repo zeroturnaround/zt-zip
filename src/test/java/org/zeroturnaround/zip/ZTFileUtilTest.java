e06a9f59bac5d5a6aa0896da2edbd7afbda16c96
Toomas Romer <toomas@zeroturnaround.com>
2015-11-30 12:14:02 +0200

    Added the license reference to the header and organised imports afterwards

--- src/test/java/org/zeroturnaround/zip/ZTFileUtilTest.java
+++ src/test/java/org/zeroturnaround/zip/ZTFileUtilTest.java
@@ -1,5 +1,19 @@
 package org.zeroturnaround.zip;
-
+/**
+ *    Copyright (C) 2012 ZeroTurnaround LLC <support@zeroturnaround.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileFilter;
@@ -7,9 +21,9 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.Collection;
 
-import junit.framework.TestCase;
-
 import org.zeroturnaround.zip.commons.FileUtils;
+
+import junit.framework.TestCase;
 
 public class ZTFileUtilTest extends TestCase {
   public void testGetTempFileFor() throws Exception {
